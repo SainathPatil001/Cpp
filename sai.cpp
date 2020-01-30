dc29f6431b2fb81de6d91f5e4fc817e48c3c1f2b
sainathpatil007 <54617839+sainathpatil007@users.noreply.github.com>
2020-01-30 01:36:53 -0800

    Add files via upload

---
+++ sai.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+
+using namespace std;
+int main()
+{
+
+ int i,j;
+
+  for(i=11;i>=7;--i)
+  {
+    for(j=7;j<=i;j++)
+    {
+      cout<<j<<" ";
+    }
+   cout<<"\n";
+  }
+
+
+ return 0;
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
