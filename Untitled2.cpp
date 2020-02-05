8b39096b4fec6d3a144f428947017f17464f5832
Sainath Sambhaji Patil <54617839+SainathPatil001@users.noreply.github.com>
2020-02-04 23:25:16 -0800

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+class first
+{
+  public:
+
+  ~first()
+
+  {
+
+  cout<<"This is the function";
+  }
+
+
+
+
+};
+
+int main()
+{
+
+  first s1;
+
+return 0;
+
+
+}
